e14153d80e69fe8f663dd3b6ddaceb5f49e3eda6
Shyam70233 <105988850+Shyam70233@users.noreply.github.com>
2022-08-15 10:36:53 +0530

    imag files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // const URL = "https://aug-6b1d4-default-rtdb.firebaseio.com/users.json";
-const URL = "https://aug-6b1d4-default-rtdb.firebaseio.com/users";
+// const URL = "https://aug-6b1d4-default-rtdb.firebaseio.com/users";
+const URL = "https://aug-6b1d4-default-rtdb.firebaseio.com/product";
 export default URL;
